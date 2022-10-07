eb6551c7fd88f46fdf859039b362a5abce7544c4
RD0407 <67634136+RD0407@users.noreply.github.com>
2022-10-07 16:42:41 +0200

    Update ML_NN_2.py

--- ML_NN_2.py
+++ ML_NN_2.py
@@ -7,4 +7,26 @@ import matplotlib.pyplot as plt # for plotting
 import matplotlib
 matplotlib.rcParams['figure.dpi']=300 # highres display
 
+# Define the parametrized nonlinear function
+def f(theta,x):
+    """
+    theta are the parameters
+    x are the input values (can be an array)
+    """
+    return(theta[0]/((x-theta[1])**2+1.0))
 
+# Define the gradient of the parametrized function
+# with respect to its parameters
+def f_grad(theta,x):
+    """
+    Return the gradient of f with respect to theta
+    shape [n_theta,n_samples]
+    where n_theta=len(theta)
+    and n_samples=len(x)
+    """
+    return(np.array([
+        1./((x-theta[1])**2+1.0)
+    ,
+        2*(x-theta[1])*theta[0]/((x-theta[1])**2+1.0)
+    ]
+    ))
