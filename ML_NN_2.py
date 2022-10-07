897d7b6e8a3c7b0f6722c52d82b0bba27f7536e6
RD0407 <67634136+RD0407@users.noreply.github.com>
2022-10-07 16:42:59 +0200

    Update ML_NN_2.py

--- ML_NN_2.py
+++ ML_NN_2.py
@@ -30,3 +30,24 @@ def f_grad(theta,x):
         2*(x-theta[1])*theta[0]/((x-theta[1])**2+1.0)
     ]
     ))
+
+
+# Define the actual function (the target, to be fitted)
+def true_f(x):
+    return( 3.0/((x-0.5)**2+1.0) )
+
+# Get randomly sampled x values
+def samples(nsamples,width=2.0):
+    return(width*np.random.randn(nsamples))
+
+# Get the average cost function on a grid of 2 parameters
+def get_avg_cost(theta0s,theta1s,nsamples):
+    n0=len(theta0s)
+    n1=len(theta1s)
+    C=np.zeros([n0,n1])
+    for j0 in range(n0):
+        for j1 in range(n1):
+            theta=np.array([theta0s[j0],theta1s[j1]])
+            x=samples(nsamples)
+            C[j0,j1]=0.5*np.average((f(theta,x)-true_f(x))**2)
+    return(C)
