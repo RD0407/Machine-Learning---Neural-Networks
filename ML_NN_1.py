698e35c46855e9a5441d6adfd77bf611c0be0239
RD0407 <67634136+RD0407@users.noreply.github.com>
2022-10-09 18:13:12 +0200

    Create ML_NN_1.py

---
+++ ML_NN_1.py
@@ -0,0 +1,4 @@
+# This notebook shows how to:
+# - implement the forward-pass (evaluation) of a deep, fully connected neural network in a few lines of python
+# - do that efficiently using batches
+# - illustrate the results for randomly initialized neural networks
