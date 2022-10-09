c6d4cea4bb3d4c5a68af6a27d70ff9d91ee01590
RD0407 <67634136+RD0407@users.noreply.github.com>
2022-10-09 18:13:42 +0200

    Update ML_NN_1.py

--- ML_NN_1.py
+++ ML_NN_1.py
@@ -2,3 +2,9 @@
 # - implement the forward-pass (evaluation) of a deep, fully connected neural network in a few lines of python
 # - do that efficiently using batches
 # - illustrate the results for randomly initialized neural networks
+
+from numpy import array, zeros, exp, random, dot, shape, reshape, meshgrid, linspace
+
+import matplotlib.pyplot as plt # for plotting
+import matplotlib
+matplotlib.rcParams['figure.dpi']=300 # highres display
