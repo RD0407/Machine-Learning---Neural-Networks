d68021e61e1f5a81d546ac74b9ac5493cc4b5fb5
RD0407 <67634136+RD0407@users.noreply.github.com>
2022-10-12 16:05:05 +0200

    Update ML_NN_1.py

--- ML_NN_1.py
+++ ML_NN_1.py
@@ -127,3 +127,11 @@ for j1 in range(M):
         y_out[j1,j2]=apply_net([value0,value1])[0]
 
  # cell 15
+
+# display image
+plt.imshow(y_out,origin='lower',extent=(-0.5,0.5,-0.5,0.5))
+plt.colorbar()
+plt.title("NN output as a function of input values")
+plt.xlabel("y_2")
+plt.ylabel("y_1")
+plt.show()
